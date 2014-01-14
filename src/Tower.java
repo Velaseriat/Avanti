7f482cbf60e07bf9bac69cea6dd98b43abd37879
aaflack18 <kmasuda@mines.edu>
2014-01-14 12:08:04 -0700

    Towers are now completely independent
    And their timers only start when they first see an enemy within their
    range = the towers no longer shoot at the same time

--- src/Tower.java
+++ src/Tower.java
@@ -12,6 +12,7 @@ public class Tower {
 	Enemy e;
 	boolean drawShot = false;
 	private int towerCount = 0;
+	private boolean counting = false;
 	
 	public Tower(int x, int y){
 		location = new Point(x, y);
@@ -20,20 +21,22 @@ public class Tower {
 	public void attack(ArrayList<Enemy> enemies){
 		numEnemiesInRange = enemies.size();
 		if (towerCount%2==0){//attack every other time
-			drawShot = true;
 			if (enemies.size() > 0){
+				counting = true;
+				drawShot = true;
 				e = enemies.get(0);
 				for (int i = 0; i < enemies.size(); i++){
 					if (e.getProgress() < enemies.get(i).getProgress())
 						e = enemies.get(i);
 				}
-				e.setHealth(e.getHealth()-100);
+				e.setHealth(e.getHealth()-80);
 			}
 		}
 		else{
 			drawShot = false;
 		}
-		towerCount += 1;
+		if (counting)
+			towerCount += 1;
 	}
 
 	public boolean isInRange(Enemy e) { //this might not be measuring quite right
@@ -43,15 +46,14 @@ public class Tower {
 	}
 	
 	public void draw(Graphics g){
-		drawShot = !drawShot;
-		g.setColor(Color.WHITE);
-		g.fillRect(50 * location.x + 25/2, 50 * location.y + 25/2, 25, 25);
 		if (drawShot && e != null && numEnemiesInRange > 0){
 			g.setColor(Color.CYAN.brighter());
 			Point l = e.getExactLocation();
 			g.drawLine(50 * location.x + 25, 50 * location.y + 25, l.x + 25, l.y + 25);
 		}
-		
+		g.setColor(Color.WHITE);
+		g.fillRect(50 * location.x + 25/2, 50 * location.y + 25/2, 25, 25);
+		drawShot = !drawShot;
 	}
 	
 	public Point getLocation(){
